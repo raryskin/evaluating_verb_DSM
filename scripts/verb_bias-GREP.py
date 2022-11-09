62bfdcfdc3def9b037d09e0feee7e32118b06506
Ellis <escred@gmail.com>
2022-11-09 08:58:27 -0800

    :q :Please enter the commit message for your changes. Lines starting

---
+++ scripts/verb_bias-GREP.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+print("init", os.getcwd())
+
+regex_comm = "grep -Ew \"^(strike|whack|hit|rub|poke|bop|smack|clean|tease|feed|scuff|pinch|knock|pat|locate|feel|spot|point|pet|look|squeeze|pick|cuddle|find|hug|select|choose).*with/\""
+
+# syntgram_dir = "../data/triarcs"
+syntgram_dir = "/home/ecain/borgstore/ecain/syntactic-ngrams/triarcs"
+target_dir = "/home/ecain/borgstore/ecain/syntactic-ngrams/filtered_triarcs"
+
+
+for filename in os.listdir(syntgram_dir):
+    if ".tsv" in filename and "filtered" not in filename:
+        print("Filename:", filename)
+
+        full_file = syntgram_dir+"/"+filename
+        output_file = target_dir + "/"+filename.replace(".tsv","_filtered.tsv")
+
+        full_comm = " ".join([regex_comm, full_file,">",output_file])
+        os.system(full_comm)
+        if os.stat(output_file).st_size == 0:
+            os.system("rm "+output_file)
+
+print("Done")
