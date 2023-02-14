dd10905543eec589a992d563fea57eb15faa3393
Ellis Cain <50685260+ellissc@users.noreply.github.com>
2023-02-14 08:39:33 -0800

    Minor updates

--- scripts/verb_bias-GREP.py
+++ scripts/verb_bias-GREP.py
@@ -3,12 +3,18 @@ import sys
 
 print("init", os.getcwd())
 
+#grep -Ew "^(strike|whack|hit|rub|poke|bop|smack|clean|tease|feed|scuff|pinch|knock|pat|locate|feel|spot|point|pet|look|squeeze|pick|cuddle|find|hug|select|choose).*with/"
+
 regex_comm = "grep -Ew \"^(strike|whack|hit|rub|poke|bop|smack|clean|tease|feed|scuff|pinch|knock|pat|locate|feel|spot|point|pet|look|squeeze|pick|cuddle|find|hug|select|choose).*with/\""
 
 # syntgram_dir = "../data/triarcs"
+# target_dir = "../data/filtered_triarcs"
+
 syntgram_dir = "/home/ecain/borgstore/ecain/syntactic-ngrams/triarcs"
 target_dir = "/home/ecain/borgstore/ecain/syntactic-ngrams/filtered_triarcs"
 
+
+print(os.listdir(syntgram_dir))
 
 for filename in os.listdir(syntgram_dir):
     if ".tsv" in filename and "filtered" not in filename:
@@ -18,6 +24,7 @@ for filename in os.listdir(syntgram_dir):
         output_file = target_dir + "/"+filename.replace(".tsv","_filtered.tsv")
 
         full_comm = " ".join([regex_comm, full_file,">",output_file])
+        print(full_comm)
         os.system(full_comm)
         if os.stat(output_file).st_size == 0:
             os.system("rm "+output_file)
--- scripts/verb_ngram_downloader.py
+++ scripts/verb_ngram_downloader.py
@@ -38,7 +38,7 @@ template_b = "-of-99.gz"
 
 os.system("mkdir ../data/triarcs/")
 
-for i in range(46, 99):
+for i in range(00, 99):
         number = f"{i:02d}"
         #print(number)
         command = "".join(["wget ",template_a, number, template_b, " -P ../data/triarcs/"])
