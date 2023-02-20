92f51081441fbb5fd33ad4bebe9ca0321555e97c
Ellis <escred@gmail.com>
2023-02-20 14:52:09 -0800

    reorg and bias graphing

---
+++ scripts/verb_bias_graphing.R
@@ -0,0 +1,130 @@
+library(here)
+setwd(here())
+library(tidyverse)
+theme_set(theme_bw() + theme(text = element_text(size = 14)))
+library(ggdist)
+library(reshape2)
+library(patchwork)
+
+ngram_df <- read_tsv(here("data_output","verb_bias_split.tsv"), show_col_type = F)
+
+ngram_df <- ngram_df |> 
+  select(-c("year_count")) |> 
+  pivot_longer(cols = starts_with("y"), 
+               names_to = "year", 
+               values_to = "count")
+
+ngram_df <- ngram_df |> 
+  mutate(year_int = as.numeric(str_sub(year, 2, 5)))
+
+for (v in unique(ngram_df$verb)){
+  print(v)
+  
+  da_g1 <- ngram_df |> 
+    filter(verb == v) |> 
+    filter(year_int >= 1900) |> 
+    ggplot(aes(x = year_int, y = count, 
+               color = alternation, group = alternation))+
+    geom_line()+
+    geom_point(aes(shape = alternation))+
+    ylab("Frequency")+
+    scale_color_manual(name = "Alternation",
+                       values = c("dobj" = "navy",
+                                  "dobj_pobj" = "red4",
+                                  "iobj_dobj" = "lightblue",
+                                  "other" = "orange"))+
+    scale_shape_manual(name = "Alternation",
+                       values = c("dobj" = 4,
+                                  "dobj_pobj" = 15,
+                                  "iobj_dobj" = 16,
+                                  "other" = 17))+
+    xlab("Year")+
+    scale_x_continuous(breaks = seq(1900, 2010, 10))+
+    ggtitle(paste("Verb:",v))
+  
+  ggsave(plot = da_g1,
+         path = here("graphs"),
+         filename = paste0(v,"_bias_raw_freq.png"),
+         dpi = 300,
+         units = "in",
+         width = 10, height = 5)
+  
+  # da_g2 <- ngram_df |> 
+  #   filter(verb == v) |> 
+  #   filter(year_int >= 1900) |> 
+  #   ggplot(aes(x = year_int, y = count, 
+  #              color = alternation, group = alternation))+
+  #   geom_line()+
+  #   geom_point(aes(shape = alternation))+
+  #   ylab("log(Frequency)")+
+  #   scale_color_manual(name = "Alternation",
+  #                      values = c("dobj" = "navy",
+  #                                 "dobj_pobj" = "red4",
+  #                                 "iobj_dobj" = "lightblue",
+  #                                 "other" = "orange"))+
+  #   scale_shape_manual(name = "Alternation",
+  #                      values = c("dobj" = 4,
+  #                                 "dobj_pobj" = 15,
+  #                                 "iobj_dobj" = 16,
+  #                                 "other" = 17))+
+  #   xlab("Year")+
+  #   scale_x_continuous(breaks = seq(1900, 2010, 10))+
+  #   ggtitle(paste("Verb:",v))+
+  #   scale_y_log10()
+  # 
+  # ggsave(plot = da_g2,
+  #        path = here("graphs"),
+  #        filename = paste0(v,"_dative_log_freq.png"),
+  #        dpi = 300,
+  #        units = "in",
+  #        width = 10, height = 5)
+  
+  da_g3 <- ngram_df |> 
+    filter(verb == v) |> 
+    filter(year_int >= 1900) |> 
+    ggplot(aes(x = year_int, y = count, 
+               fill = alternation))+
+    geom_bar(stat = "identity", position = "fill", width = 1)+
+    ylab("Proportion")+
+    scale_fill_manual(name = "Alternation",
+                      values = c("dobj" = "navy",
+                                 "dobj_pobj" = "red4",
+                                 "iobj_dobj" = "lightblue",
+                                 "other" = "orange"))+
+    xlab("Year")+
+    scale_x_continuous(breaks = seq(1900, 2010, 10))+
+    ggtitle(paste("Verb:",v))
+  
+  ggsave(plot = da_g3,
+         path = here("graphs"),
+         filename = paste0(v,"_bias_bar_prop.png"),
+         dpi = 300,
+         units = "in",
+         width = 10, height = 5)
+  
+  
+  
+}
+
+# v = "sneak"
+# ngram_df |>
+#   filter(verb == v) |>
+#   filter(year_int >= 1900) |> 
+#   ggplot(aes(x = year_int, y = count,
+#              color = alternation, group = alternation))+
+#   geom_line()+
+#   geom_point(aes(shape = alternation))+
+#   ylab("Frequency")+
+#   scale_color_manual(name = "Alternation",
+#                      values = c("dobj" = "navy",
+#                                 "dobj_pobj" = "red4",
+#                                 "iobj_dobj" = "lightblue",
+#                                 "other" = "orange"))+
+#   scale_shape_manual(name = "Alternation",
+#                      values = c("dobj" = 4,
+#                                 "dobj_pobj" = 15,
+#                                 "iobj_dobj" = 16,
+#                                 "other" = 17))+
+#   xlab("Year")+
+#   scale_x_continuous(breaks = seq(1900, 2010, 10))+
+#   ggtitle(paste("Verb:",v))
---
+++ scripts/verb_bias_years.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import json
+import os
+import csv
+import time
+import sys
+from collections import Counter
+
+pd.options.mode.chained_assignment = None
+
+year_grid = pd.DataFrame()
+
+result = pd.read_csv("../data_output/verb_bias.tsv", sep='\t')
+
+print(result)
+
+def get_cols(input_string, lower = 1800, upper = 2008):
+    print("Starting")
+    if input_string != "-1":
+        a = input_string.replace("[(","").replace(")]","")
+        print(a)
+        year_dict = {}
+        for item in a.split("), ("):
+            set = item.replace("'","").split(", ")
+            year_dict[set[0]] = int(set[1])
+
+        dict_to_add = {}
+
+        for i in range(lower, upper+1):
+            i = str(i)
+            if i in year_dict.keys():
+                dict_to_add["y"+i] = year_dict[i]
+            else:
+                dict_to_add["y"+i] = 0
+    else:
+        dict_to_add = {}
+
+        for i in range(lower, upper+1):
+            i = str(i)
+            dict_to_add["y"+i] = 0
+
+
+    return dict_to_add
+
+years = []
+
+for i in range(1800, 2008+1):
+    years.append("y"+str(i))
+
+## issues with fragmented dataframe
+result[years] = result.apply(lambda x: pd.Series(get_cols(x["year_count"])), axis = 1)
+
+print("de-fragmented frame")
+unfragmented = result.copy()
+print(unfragmented)
+
+unfragmented.to_csv("../data_output/verb_bias_split.tsv", sep = "\t", index = False)
